64fb1d266deee844cecbfab1fd847b083f74b2d6
myapptly <tbledsoe1954@gmail.com>
2026-07-01 11:18:53 -0400

    Update navbar logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-[#0D0D0D]/90 backdrop-blur-sm border-b border-white/10">
       <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
         <Image
-          src="https://galaxy-prod.tlcdn.com/gen/user_37MJjcymE8bOUM2E9geQUee0w5V/3341da3f-c232-4e65-89a7-0a9e768e55ef.png"
+          src="/small%20apptly%20logo.png" 
           alt="APPTLY Logo"
           width={80}
           height={80}
