63d5f7b080a7aff5faa9007a98b8f2ac19864184
myapptly <tbledsoe1954@gmail.com>
2026-07-04 18:27:03 -0400

    Update page

--- app/finish/page.tsx
+++ app/finish/page.tsx
@@ -7,7 +7,7 @@ export default function FinishPage() {
   alt="Congratulations"
   className="w-full rounded-3xl shadow-2xl mb-8"
 /> 
-        
+        <a
         href="/dashboard"
             className="bg-[#00FF94] text-black font-bold px-6 py-4 rounded-full"
           >
