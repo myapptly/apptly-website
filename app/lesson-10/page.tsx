d48916b536510442a7718ab4f580a7d2e7b13df1
myapptly <tbledsoe1954@gmail.com>
2026-07-05 10:10:44 -0400

    Update page

--- app/lesson-10/page.tsx
+++ app/lesson-10/page.tsx
@@ -24,8 +24,8 @@ export default function Lesson10() {
 
         <div className="my-10">
   <img
-    src="/apptly flow.png"
-    alt="The APPTLY Journey"
+    src="/your next app.png"
+    alt="Your Next App"
     className="w-full rounded-2xl border border-[#00FF94]/30 shadow-2xl"
   />
 </div> 
