e24fe035c0d92b9ea82b60777677acb65154b2c2
myapptly <tbledsoe1954@gmail.com>
2026-07-17 15:47:14 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,6 +35,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased`}>
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-PMDMDBH2LF"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-PMDMDBH2LF');
+  `}
+</Script> 
         {children}
       </body>
     </html>
