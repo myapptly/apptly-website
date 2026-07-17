0b7659787ae43208d4737d3689191fdfdac1ddcb
myapptly <tbledsoe1954@gmail.com>
2026-07-17 14:35:36 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     siteName: "APPTLY",
     images: [
       {
-        url: "https://myapptly.com/og-image.png",
+        url: "https://myapptly.com/apptly%20logo.png", 
         width: 1200,
         height: 630,
       },
