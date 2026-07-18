4ccb3c5175741b4148fc4d1577c02c5d93ee4754
myapptly <tbledsoe1954@gmail.com>
2026-07-18 15:06:57 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ export function Navbar() {
   alt="APPTLY Logo"
   width={300}
   height={75}
-  className="object-contain w-[200px] h-auto"
+  className="object-contain w-[180px] h-auto"
 /> 
         <a
           href="#pricing"
