9d7caf6d46c1bef9051aa26966d9d7f810f4d13d
myapptly <tbledsoe1954@gmail.com>
2026-08-04 17:11:41 -0400

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,18 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#10b981',
     icons: [
       {
-        src: '/icon-192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: '/apple-icon.png',
-        sizes: '180x180',
+        src: '/apptly%20real%20logo.png', // or '/apptly-real-logo.png' if renamed
+        sizes: 'any',
         type: 'image/png',
       },
     ],
