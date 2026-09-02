9244ef95d85726db9e2534964b06abc3bb28bb30
myapptly <tbledsoe1954@gmail.com>
2026-09-02 10:30:04 -0400

    Update sitemap URLs to remove 'www'

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.myapptly.com",
+      url: "https://myapptly.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.myapptly.com/start-your-project",
+      url: "https://myapptly.com/start-your-project",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://www.myapptly.com/privacy",
+      url: "https://myapptly.com/privacy",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.3,
     },
     {
-      url: "https://www.myapptly.com/terms",
+      url: "https://myapptly.com/terms",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.3,
